7391e6eb296fd6bfa84f57e5a315d95cb67844b9
Suvendu Panda <suvendu_panda1@epam.com>
2025-03-05 16:33:30 +0530

    .

--- task03/jsrc/main/java/com/task03/HelloWorld.java
+++ task03/jsrc/main/java/com/task03/HelloWorld.java
@@ -3,8 +3,8 @@ package com.task03;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
-import com.syndicate.deployment.model.RetentionSetting;
 import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
+import com.syndicate.deployment.model.RetentionSetting;
 import com.syndicate.deployment.model.lambda.url.AuthType;
 
 import java.util.HashMap;
@@ -14,40 +14,25 @@ import java.util.Map;
 		lambdaName = "hello_world",
 		roleName = "hello_world-role",
 		isPublishVersion = true,
-		aliasName = "learn",
+		aliasName = "${lambdas_alias_name}",
 		logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
-@LambdaUrlConfig(authType = AuthType.NONE)
-public class HelloWorld implements RequestHandler<Map<String, Object>, Map<String, Object>> {
+@LambdaUrlConfig(authType = AuthType.NONE )
+public class HelloWorld implements RequestHandler<Map<String , Object>, Map<String, Object>> {
 
-	@Override
-	public Map<String, Object> handleRequest(Map<String, Object> event, Context context) {
+	public Map<String, Object> handleRequest(Map<String , Object> event, Context context) {
 		System.out.println("Hello from lambda");
+		String path = (String) event.getOrDefault("rawPath", event.get("path"));
+		Map<String, Object> requestContext = (Map<String, Object>) event.get("requestContext");
+		Map<String, Object> http = requestContext != null ? (Map<String, Object>) requestContext.get("http") : null;
+		String method = http != null ? (String) http.get("method") : (String) event.get("httpMethod");
 
-
-		String path = event.containsKey("rawPath") ? (String) event.get("rawPath") : (String) event.get("path");
-		if (path == null) path = "/";
-
-
-		String method = "UNKNOWN";
-		if (event.containsKey("requestContext")) {
-			Map<String, Object> requestContext = (Map<String, Object>) event.get("requestContext");
-			if (requestContext.containsKey("http")) {
-				Map<String, Object> http = (Map<String, Object>) requestContext.get("http");
-				if (http.containsKey("method")) {
-					method = (String) http.get("method");
-				}
-			}
-		} else if (event.containsKey("httpMethod")) {
-			method = (String) event.get("httpMethod");
-		}
 		if ("/hello".equals(path) && "GET".equalsIgnoreCase(method)) {
 			return createSuccessResponse();
 		} else {
 			return createErrorResponse("Bad request syntax or unsupported method. Request path: " + path + ". HTTP method: " + method);
 		}
 	}
-
 	private Map<String, Object> createSuccessResponse() {
 		Map<String, Object> response = new HashMap<>();
 		response.put("statusCode", 200);
