ee054397ee287192899a6fcc6e228b5ab13f5480
Suvendu Panda <suvendu_panda1@epam.com>
2025-03-06 22:11:32 +0530

    .

--- task05/jsrc/main/java/com/task05/ApiHandler.java
+++ task05/jsrc/main/java/com/task05/ApiHandler.java
@@ -4,6 +4,9 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+import software.amazon.awssdk.services.dynamodb.model.PutItemRequest;
 
 import java.time.Instant;
 import java.util.HashMap;
@@ -19,6 +22,10 @@ import java.util.UUID;
 )
 public class ApiHandler implements RequestHandler<Map<String, Object>, Map<String, Object>> {
 
+	private final DynamoDbClient dynamoDb = DynamoDbClient.create();
+	private static final String TABLE_NAME = "cmtr-57a5b864-Events-3tt3";
+
+	@Override
 	public Map<String, Object> handleRequest(Map<String, Object> request, Context context) {
 		context.getLogger().log("Received request: " + request.toString());
 
@@ -29,6 +36,16 @@ public class ApiHandler implements RequestHandler<Map<String, Object>, Map<Strin
 		// Extract request parameters
 		Integer principalId = (Integer) request.get("principalId");
 		Map<String, Object> content = (Map<String, Object>) request.get("content");
+
+		// Prepare item for DynamoDB
+		Map<String, AttributeValue> item = new HashMap<>();
+		item.put("id", AttributeValue.builder().s(id).build());
+		item.put("principalId", AttributeValue.builder().n(String.valueOf(principalId)).build());
+		item.put("createdAt", AttributeValue.builder().s(timestamp).build());
+		item.put("body", AttributeValue.builder().s(content.toString()).build());
+
+		// Store in DynamoDB
+		dynamoDb.putItem(PutItemRequest.builder().tableName(TABLE_NAME).item(item).build());
 
 		// Prepare response
 		Map<String, Object> response = new HashMap<>();
